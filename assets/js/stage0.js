c1ee997625326467631810ea45cf6a868c7a29a2
kooomay <89886849+kooomay@users.noreply.github.com>
2021-09-14 16:53:41 +0900

    Add files via upload

---
+++ assets/js/stage0.js
@@ -0,0 +1,11 @@
+function answer()
+        {
+          // "こうもり"の部分を変えて答えを変更
+          if(form.ans_input.value=="こうもり"){
+          window.location.href = 'loading-ok.html';
+        }
+          else{
+           window.location.href = 'loading-return.html';
+          }
+        }
+
