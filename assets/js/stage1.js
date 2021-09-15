775c9ee208aec33729320e17474f4c34466bf080
kooomay <89886849+kooomay@users.noreply.github.com>
2021-09-15 11:55:09 +0900

    Add files via upload

---
+++ assets/js/stage1.js
@@ -0,0 +1,11 @@
+function answer()
+        {
+          // "こうもり"の部分を変えて答えを変更
+          if(form.ans_input.value=="こうもり"){
+          window.location.href = 'stage1-2.html';
+        }
+          else{
+           window.location.href = 'else-1.html';
+          }
+        }
+
---
+++ assets/js/stage2.js
@@ -0,0 +1,11 @@
+function answer()
+        {
+          // "こうもり"の部分を変えて答えを変更
+          if(form.ans_input.value=="こうもり"){
+          window.location.href = 'stage2-3.html';
+        }
+          else{
+           window.location.href = 'else-2.html';
+          }
+        }
+
