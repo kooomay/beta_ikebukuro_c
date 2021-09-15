cbaabf33e6b5d7ac51666f0e86b53f0dd53dc596
kooomay <89886849+kooomay@users.noreply.github.com>
2021-09-16 01:53:35 +0900

    Update stage3.js

--- assets/js/stage3.js
+++ assets/js/stage3.js
@@ -1,8 +1,8 @@
 function answer()
         {
           // "こうもり"の部分を変えて答えを変更
-          if(form.ans_input.value=="こうもり"){
-          window.location.href = 'stage1-2.html';
+          if(form.ans_input.value=="さくじょ"){
+          window.location.href = 'final.html';
         }
           else{
            window.location.href = '.html';
