7f4b91a04af2724ceabbffd7aba548605b3151fa
kooomay <89886849+kooomay@users.noreply.github.com>
2021-09-17 15:48:52 +0900

    Update stage1.js

--- assets/js/stage1.js
+++ assets/js/stage1.js
@@ -1,11 +1,16 @@
-function answer()
-        {
-          // "こうもり"の部分を変えて答えを変更
-          if(form.ans_input.value=="あおきあか"){
-          window.location.href = 'stage1-2.html';
-        }
-          else{
-           window.location.href = 'else-1.html';
-          }
-        }
+function answer(){
+    var input_answer_1 = document.getElementById('ans_input_1'); 
+    var input_answer_2 = document.getElementById('ans_input_2'); 
+    var input_answer_3 = document.getElementById('ans_input_3'); 
 
+    const Answer1 = input_answer_1.value ;
+    const Answer2 = input_answer_2.value ;
+    const Answer3 = input_answer_3.value ;
+    
+    if(Answer1==1&&Answer2==3&&Answer3==2){
+        window.location.href = 'stage2.html';
+    }
+    else{
+        window.location.href = 'stage3.html';
+    }
+}
